d72e9457af017e7e1a4a90357bab773a6845e863
Néstor Trillo <nospam@avaniu.com>
2021-03-19 10:30:05 -1000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nestor Trillo on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
